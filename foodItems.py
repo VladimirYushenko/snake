06840acfb7b543d26c956cdf05ea4c985c15fe84
Vladimir Yushenko <vladimir.yushenko@gmail.com>
2023-05-29 14:55:19 -0700

    eating and adding

--- foodItems.py
+++ foodItems.py
@@ -1,13 +1,17 @@
-import pygame, foodItem
+import pygame, foodItem, random
 class FoodItems:
 
-    def __init__(self, blockWidth, blockHeight):
+    def __init__(self, blockWidth, blockHeight, columns, rows):
         self.head = None
         self.blockWidth = blockWidth
         self.blockHeight = blockHeight
+        self.columns = columns
+        self.rows = rows
 
-    def addFoodItem(self, column, row):
-        newFoodItem = foodItem.FoodItem(column, row, self.blockWidth, self.blockHeight)
+    def addFoodItem(self):
+        foodCol = round(random.random() * self.columns)
+        foodRow = round(random.random() * self.rows)
+        newFoodItem = foodItem.FoodItem(foodCol, foodRow, self.blockWidth, self.blockHeight)
         if self.head == None:
             self.head = newFoodItem
         else:
@@ -23,6 +27,28 @@ class FoodItems:
             currentItem = currentItem.next
         
         return currentItem
+    
+    # Detects and returns if snake touches any food item, and removes it 
+    def eat(self, headCol, headRow):
+        currentItem = self.head
+        while currentItem != None:
+            if headCol == currentItem.column and headRow == currentItem.row:
+                self.removeItem(currentItem)
+                return True
+            currentItem = currentItem.next
+        
+        return False
+    
+    def removeItem(self, nodeToRemove: foodItem.FoodItem):
+        if nodeToRemove == self.head:
+            self.head = self.head.next
+        else:
+            currentItem = self.head
+            while currentItem != None:
+                if currentItem.next == nodeToRemove:
+                    currentItem.next = nodeToRemove.next
+                currentItem = currentItem.next
+
     
     def render(self, screen: pygame.Surface):
         currentItem = self.head
--- playScreen.py
+++ playScreen.py
@@ -1,4 +1,4 @@
-import pygame, board, snake, constants, foodItems
+import pygame, board, snake, constants, foodItems, random
 import gameText
 class PlayScreen:
     def __init__(self, gameBoard: board.Board, mainSnake: snake.Snake):
@@ -6,12 +6,12 @@ class PlayScreen:
         self.black = 0, 0, 0
         self.gameBoard = gameBoard
         self.mainSnake = mainSnake
-        self.loopCounter = 0
+        self.frameCounter = 0
         #self.foodItem1 = foodItem.FoodItem(2, 1, gameBoard.SQUARE_WIDTH, gameBoard.SQUARE_HEIGHT)
         #self.foodItem2 = foodItem.FoodItem(3, 6, gameBoard.SQUARE_WIDTH, gameBoard.SQUARE_HEIGHT)
-        self.foodItems = foodItems.FoodItems(gameBoard.SQUARE_WIDTH, gameBoard.SQUARE_HEIGHT)
-        self.foodItems.addFoodItem(2, 1)
-        self.foodItems.addFoodItem(3, 6)
+        self.foodItems = foodItems.FoodItems(gameBoard.SQUARE_WIDTH, gameBoard.SQUARE_HEIGHT, gameBoard.columns, gameBoard.rows)
+        self.foodItems.addFoodItem()
+        self.foodItems.addFoodItem()
 
     def render(self, screen: pygame.Surface):
         screen.fill(self.black)
@@ -25,13 +25,24 @@ class PlayScreen:
         pygame.display.flip()
     
     def process(self):
-        self.loopCounter = self.loopCounter + 1
-        if self.loopCounter == 10:
-            self.loopCounter = 0
+        self.frameCounter = self.frameCounter + 1
+        if self.frameCounter == 10:
+            self.frameCounter = 0
+            # Try to add more food
+            if random.random() < 0.015:
+                self.foodItems.addFoodItem()
+
+            # Move the snake
             self.mainSnake.moveSnake()
+
+            # Check if snake died
             if self.mainSnake.hasDied():
                 self.mainSnake.initialize()
                 return constants.GAME_STATE_SPLASH
+            
+            # Check if food item was eaten
+            if self.foodItems.eat(self.mainSnake.snake[0][0], self.mainSnake.snake[0][1]):
+                self.mainSnake.setGrowNextMove()
 
         if pygame.key.get_pressed()[pygame.K_w]:
             self.mainSnake.changeDirection(snake.Snake.UP)
@@ -41,8 +52,6 @@ class PlayScreen:
             self.mainSnake.changeDirection(snake.Snake.LEFT)
         elif pygame.key.get_pressed()[pygame.K_d]:
             self.mainSnake.changeDirection(snake.Snake.RIGHT)
-        elif pygame.key.get_pressed()[pygame.K_SPACE]:
-            self.mainSnake.setGrowNextMove()
         elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
             return constants.GAME_STATE_PAUSE
     
